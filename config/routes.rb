e71705f981d7fa4fc4b6b07168e27d66b27d3b51
Ramzi Deek <ramzideek@gmail.com>
2016-09-01 16:23:12 -0500

    Create initial navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root "pages#home"
-  get 'pages/home'
+  get "about" => "pages#about"
+  get "contact" => "pages#contact"
+  get "training" => "pages#training"
   end
